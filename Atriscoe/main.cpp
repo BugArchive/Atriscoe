87575febe36d6f003583df67fbba5e0b8d80ad45
Alex <BugArchive@outlook.com>
2020-05-06 15:22:24 +0200

    Modify window settings

--- Atriscoe/main.cpp
+++ Atriscoe/main.cpp
@@ -1,19 +1,27 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Window");
-	sf::CircleShape circle(100.0f);
-	circle.setFillColor(sf::Color::Green);
+	sf::ContextSettings context_settings;
+	context_settings.antialiasingLevel = 8;
+	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "SFML Window", sf::Style::Fullscreen, context_settings);
+	window.setFramerateLimit(60);
+	bool window_focus = true;
 
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event));
 		{
 			if (event.type == sf::Event::Closed) window.close();
+
+			if(event.type == sf::Event::GainedFocus) window_focus = true;
+
+			if(event.type == sf::Event::LostFocus) window_focus = false;
+		}
+		if (window_focus) {
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window.close();
 		}
 
 		window.clear();
-		window.draw(circle);
 		window.display();
 	}
 	return 0;
